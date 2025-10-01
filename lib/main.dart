af9309922a59da3613469bef0270ef68ea1dade1
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-01 18:59:13 -0400

    added a rotating image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart'; // <-- add this
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ------------------- First Screen -------------------
 class FadingTextAnimation extends StatefulWidget {
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
@@ -21,7 +22,7 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
-  Color _textColor = Colors.black; // <-- text color state
+  Color _textColor = Colors.black;
 
   void toggleVisibility() {
     setState(() {
@@ -69,7 +70,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
         title: Text('Fading Text Animation'),
         actions: [
           IconButton(
-            icon: Icon(Icons.color_lens), // <-- color picker icon
+            icon: Icon(Icons.color_lens),
             onPressed: _pickColor,
           ),
         ],
@@ -80,7 +81,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
           duration: Duration(seconds: 1),
           child: Text(
             'Hello, Flutter!',
-            style: TextStyle(fontSize: 24, color: _textColor), // <-- apply color
+            style: TextStyle(fontSize: 24, color: _textColor),
           ),
         ),
       ),
@@ -92,14 +93,45 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   }
 }
 
+// ------------------- Second Screen -------------------
 class SecondFadingTextAnimation extends StatefulWidget {
   @override
-  _SecondFadingTextAnimationState createState() => _SecondFadingTextAnimationState();
+  _SecondFadingTextAnimationState createState() =>
+      _SecondFadingTextAnimationState();
 }
 
-class _SecondFadingTextAnimationState extends State<SecondFadingTextAnimation> {
+class _SecondFadingTextAnimationState extends State<SecondFadingTextAnimation>
+    with SingleTickerProviderStateMixin {
   bool _isVisible = true;
-  Color _textColor = Colors.blue; // <-- text color state
+  Color _textColor = Colors.blue;
+
+  late AnimationController _controller;
+  late Animation<double> _rotation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Rotation controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds:1),
+    );
+
+    // Apply easeIn curve
+    _rotation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.repeat(); // keeps rotating forever
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void toggleVisibility() {
     setState(() {
@@ -147,19 +179,34 @@ class _SecondFadingTextAnimationState extends State<SecondFadingTextAnimation> {
         title: Text('Second Fading Animation'),
         actions: [
           IconButton(
-            icon: Icon(Icons.color_lens), // <-- color picker icon
+            icon: Icon(Icons.color_lens),
             onPressed: _pickColor,
           ),
         ],
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 3),
-          child: Text(
-            'This is the second screen!',
-            style: TextStyle(fontSize: 24, color: _textColor), // <-- apply color
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 3),
+              child: Text(
+                'This is the second screen!',
+                style: TextStyle(fontSize: 24, color: _textColor),
+              ),
+            ),
+            SizedBox(height: 40),
+            RotationTransition(
+              turns: _rotation,
+              child: Image.network(
+                'https://plus.unsplash.com/premium_photo-1673643260141-2fa3c98ef333?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                width: 300,
+                height: 300,
+                
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -170,14 +217,15 @@ class _SecondFadingTextAnimationState extends State<SecondFadingTextAnimation> {
   }
 }
 
-// Wrap both screens inside PageView
+// ------------------- PageView -------------------
 class SwipeScreens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      scrollDirection: Axis.horizontal, // swipe left/right
       children: [
-        FadingTextAnimation(),        // First screen
-        SecondFadingTextAnimation(),  // Second screen
+        FadingTextAnimation(), // First screen
+        SecondFadingTextAnimation(), // Second screen
       ],
     );
   }
