ba4aef8d2d3d973aabf8708a795f874bc0a84230
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-01 18:29:57 -0400

    added a color picker for the text and added a second screen that you can swipe to

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart'; // <-- add this
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      home: SwipeScreens(),
     );
   }
 }
@@ -20,6 +21,7 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color _textColor = Colors.black; // <-- text color state
 
   void toggleVisibility() {
     setState(() {
@@ -27,11 +29,50 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     });
   }
 
+  void _pickColor() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        Color tempColor = _textColor;
+        return AlertDialog(
+          title: Text('Pick a text color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: tempColor,
+              onColorChanged: (color) {
+                tempColor = color;
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text('Select'),
+              onPressed: () {
+                setState(() => _textColor = tempColor);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.color_lens), // <-- color picker icon
+            onPressed: _pickColor,
+          ),
+        ],
       ),
       body: Center(
         child: AnimatedOpacity(
@@ -39,7 +80,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
           duration: Duration(seconds: 1),
           child: Text(
             'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+            style: TextStyle(fontSize: 24, color: _textColor), // <-- apply color
           ),
         ),
       ),
@@ -47,6 +88,97 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
         onPressed: toggleVisibility,
         child: Icon(Icons.play_arrow),
       ),
+    );
+  }
+}
+
+class SecondFadingTextAnimation extends StatefulWidget {
+  @override
+  _SecondFadingTextAnimationState createState() => _SecondFadingTextAnimationState();
+}
+
+class _SecondFadingTextAnimationState extends State<SecondFadingTextAnimation> {
+  bool _isVisible = true;
+  Color _textColor = Colors.blue; // <-- text color state
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  void _pickColor() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        Color tempColor = _textColor;
+        return AlertDialog(
+          title: Text('Pick a text color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: tempColor,
+              onColorChanged: (color) {
+                tempColor = color;
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text('Select'),
+              onPressed: () {
+                setState(() => _textColor = tempColor);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Fading Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.color_lens), // <-- color picker icon
+            onPressed: _pickColor,
+          ),
+        ],
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 3),
+          child: Text(
+            'This is the second screen!',
+            style: TextStyle(fontSize: 24, color: _textColor), // <-- apply color
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+// Wrap both screens inside PageView
+class SwipeScreens extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        FadingTextAnimation(),        // First screen
+        SecondFadingTextAnimation(),  // Second screen
+      ],
     );
   }
 }
